8924a9782c890db9a01a7103d844601a771031eb
Hero <Hero>
2022-03-16 15:30:56 +0700

    feat: Create dashboard controller #12

---
+++ source/src/main/java/com/javabootcamp/assessment2/features/dashboard/DashboardController.java
@@ -0,0 +1,23 @@
+package com.javabootcamp.assessment2.features.dashboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/secure/dashboard")
+public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public DashboardResponse getDashboardOverview(@RequestParam UUID shipmentId) {
+        DashboardResponse dashboardOverview = dashboardService.getDashboardOverview(shipmentId);
+        return dashboardOverview;
+    }
+
+}
---
+++ source/src/main/java/com/javabootcamp/assessment2/features/dashboard/DashboardControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.javabootcamp.assessment2.features.dashboard;
+
+import com.javabootcamp.assessment2.features.asset.AssetNotFoundException;
+import com.javabootcamp.assessment2.features.shipment.ShipmentNotFoundException;
+import com.javabootcamp.assessment2.features.shipment.ShipmentTypeInvalidException;
+import com.javabootcamp.assessment2.views.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class DashboardControllerAdvice {
+    @ExceptionHandler(ShipmentNotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse shipmentNotFound(ShipmentNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ShipmentTypeInvalidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse shipmentTypeInvalid(ShipmentTypeInvalidException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+}
