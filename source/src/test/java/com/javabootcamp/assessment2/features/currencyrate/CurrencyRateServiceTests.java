3f8b664fa3f2be072aa78b8179e0e4650d64c8fc
Hero <Hero>
2022-03-18 10:49:47 +0700

    test: Create currency rate service test #27

--- source/src/test/java/com/javabootcamp/assessment2/features/currencyrate/CurrencyRateServiceTests.java
+++ source/src/test/java/com/javabootcamp/assessment2/features/currencyrate/CurrencyRateServiceTests.java
@@ -1,23 +1,52 @@
 package com.javabootcamp.assessment2.features.currencyrate;
+import com.javabootcamp.assessment2.entities.CurrencyRate;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class CurrencyRateServiceTests {
 
     private CurrencyRateService testService;
 
+    @Mock
+    private CurrencyRateRepository currencyRateRepository;
+
     @BeforeEach
     void setup() {
 
+        testService = new CurrencyRateService(currencyRateRepository);
     }
 
     @Test
+    @DisplayName("Verify convertCurrencyAmountToTHB that should be succes")
     void test1() {
-        assertEquals(1, 1);
+        var rates = new ArrayList<CurrencyRate>();
+        var usdRate = new CurrencyRate();
+        usdRate.setRate(30);
+        usdRate.setCode("USD");
+        rates.add(usdRate);
+        var result = testService.convertCurrencyAmountToTHB(rates, "USD", 10);
+        assertEquals(300, result);
+    }
+
+    @Test
+    @DisplayName("Verify convertCurrencyAmountToTHB that should be throw CurrencyRateNotFoundException")
+    void test2() {
+        var rates = new ArrayList<CurrencyRate>();
+        var usdRate = new CurrencyRate();
+        usdRate.setRate(30);
+        usdRate.setCode("TEST");
+        rates.add(usdRate);
+        assertThrows(CurrencyRateNotFoundException.class,
+                () -> testService.convertCurrencyAmountToTHB(rates, "USD", 10));
     }
 }
